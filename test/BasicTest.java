42070ba33430ebe96792010336340e127d655e05
Pavel Derendyaev <paul@reic.ru>
2014-04-30 18:07:36 +0400

    Add JDK8 test

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -2,15 +2,25 @@ import models.Message;
 import org.junit.*;
 import play.test.*;
 
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 
-public class BasicTest extends UnitTest {
-
+public class BasicTest extends UnitTest
+{
     @Test
     public void testModel()
     {
         Message msg = Message.findById( 1L );
         assertNotNull( msg );
-        assertThat( msg.text, is( "Hello, world" ));
+        assertThat( msg.text, is( "Hello, world" ) );
+    }
+
+    @Test
+    public void testJdk8()
+    {
+        List<Message> messages = Message.findAll();
+        long count = messages.stream().filter( msg -> msg.text.startsWith( "H" )).count();
+        assertThat( count, is( 1L ));
     }
 }
