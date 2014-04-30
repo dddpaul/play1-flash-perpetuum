9f07d47e22dd63583ac3ba7eed18c05e5e24d049
Pavel Derendyaev <paul@reic.ru>
2014-04-30 20:18:39 +0400

    Add Jdk8 test to ApplicationSpec. To prevent https://github.com/codeborne/play-tests/issues/4 one needs to set "systemProperties['precompiled'] = 'false'" in play-tests' build.gradle.
    And don't forget to set "sourceCompatibility = 1.8"

--- test/ui/ApplicationSpec.java
+++ test/ui/ApplicationSpec.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import play.test.UITest;
 
+import java.util.Arrays;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.hamcrest.core.Is.is;
 
 public class ApplicationSpec extends UITest
 {
@@ -20,6 +23,13 @@ public class ApplicationSpec extends UITest
     @Test
     public void index()
     {
+        $( By.name( "submit" ) ).shouldHave( text( "Add" ) );
+    }
+
+    @Test
+    public void indexJdk8()
+    {
+        assertThat( Arrays.asList( "1", "2" ).stream().map( Integer::parseInt ).count(), is( 2L ));
         $( By.name( "submit" ) ).shouldHave( text( "Add" ) );
     }
 }
